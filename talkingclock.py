77042ab866a149d64c2510d185e1b22bae63828b
Enzo C <125571218+LunaticBH@users.noreply.github.com>
2023-08-04 09:36:41 -0400

    Add files via upload

--- talkingclock.py
+++ talkingclock.py
@@ -35,26 +35,28 @@ class Solution:
             hour = {1:"one", 2:"two", 3:"three", 4:"four", 5:"five", 6:"six", 7:"seven", 8:"eight", 9:"nine", 10:"ten", 11:"eleven", 12:"twelve", 13:"one", 14:"two", 15:"three", 16:"four", 17:"five", 18:"six", 19:"seven", 20:"eight", 21:"nine", 22:"ten", 23:"eleven", 00:"twelve"}
             min = {10:"ten", 11:"eleven", 12:"twelve", 13:"thirteen", 14:"fourteen", 15:"fifteen", 16:"sixteen", 17:"seventeen", 18:"eighteen", 19:"nineteen", 20:"twenty", 30:"thirty", 40:"forty", 50:"fifty"}
 
-            h = int(str(input_time[0] + input_time[1]))
-            m = int(str(input_time[3] + input_time[4]))
+            h = int(input_time[0:1])
+            tm = int(input_time[3:4])
+            m = int(input_time[4])
+
             if h >= 12:
                 if input_time[3] == "0" and input_time[4] == "0":
                     return f"It's {hour[h]} pm"
                 elif input_time[3] == "0" and input_time[4] != "0":
-                    return f"It's {hour[h]} oh {hour[h]} pm"
+                    return f"It's {hour[h]} oh {hour[m]} pm"
                 elif input_time[3] == "1":
-                    return f"It's {hour[h]} {min[m]} pm"
+                    return f"It's {hour[h]} {min[tm]} pm"
                 elif input_time[3] == "2" or "3" or "4" or "5":
-                    return f"It's {hour[h]} {min[m]} {hour[int(input_time[4])]} pm"
+                    return f"It's {hour[h]} {min[tm]} {hour[m]} pm"
             else:
                 if input_time[3] == "0" and input_time[4] == "0":
                     return f"It's {hour[h]} am"
                 elif input_time[3] == "0" and input_time[4] != "0":
-                    return f"It's {hour[h]} oh {hour[h]} am"
+                    return f"It's {hour[h]} oh {hour[m]} am"
                 elif input_time[3] == "1":
-                    return f"It's {hour[h]} {min[m]} am"
+                    return f"It's {hour[h]} {min[tm]} am"
                 elif input_time[3] == "2" or "3" or "4" or "5":
-                    return f"It's {hour[h]} {min[m]} {hour[int(input_time[4])]} am"
+                    return f"It's {hour[h]} {min[tm]} {hour[m]} am"
 
 
 def main():
